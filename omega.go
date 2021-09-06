a1cd043c025abb12553ce311c49f1e76edf6317c
Yeti Sno <yeti@yetiz.org>
2021-09-06 12:54:24 +0800

    add As<Class>

--- omega.go
+++ omega.go
@@ -23,3 +23,43 @@ func (x *TransitFrame) Clone() *TransitFrame {
 		Data:             x.GetData(),
 	}
 }
+
+func (x *TransitFrame) SetData(data isTransitFrame_Data) *TransitFrame {
+	x.Data = data
+	return x
+}
+
+func (x *TransitFrame) AsRequest() *TransitFrame {
+	x.Class = TransitFrame_ClassRequest
+	return x
+}
+
+func (x *TransitFrame) AsResponse() *TransitFrame {
+	x.Class = TransitFrame_ClassResponse
+	return x
+}
+
+func (x *TransitFrame) AsRequestReplay() *TransitFrame {
+	x.Class = TransitFrame_ClassRequestReplay
+	return x
+}
+
+func (x *TransitFrame) AsResponseReplay() *TransitFrame {
+	x.Class = TransitFrame_ClassResponseReplay
+	return x
+}
+
+func (x *TransitFrame) AsNotification() *TransitFrame {
+	x.Class = TransitFrame_ClassNotification
+	return x
+}
+
+func (x *TransitFrame) AsNotificationReplay() *TransitFrame {
+	x.Class = TransitFrame_ClassNotificationReplay
+	return x
+}
+
+func (x *TransitFrame) AsError() *TransitFrame {
+	x.Class = TransitFrame_ClassError
+	return x
+}
