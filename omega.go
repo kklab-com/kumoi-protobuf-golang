afa920491f3e8f31b93a409adc2e1737e425fee5
Yeti Sno <yeti@yetiz.org>
2021-09-06 12:50:12 +0800

    add Clone()

--- omega.go
+++ omega.go
@@ -1,10 +1,25 @@
 package omega
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 type TransitFrameData = isTransitFrame_Data
 
 func (x *Error) Error() string {
 	bs, _ := json.Marshal(x)
 	return string(bs)
+}
+
+func (x *TransitFrame) Clone() *TransitFrame {
+	return &TransitFrame{
+		TransitId:        x.GetTransitId(),
+		Class:            x.GetClass(),
+		Version:          x.GetVersion(),
+		Timestamp:        x.GetTimestamp(),
+		Error:            x.GetError(),
+		MessageId:        x.GetMessageId(),
+		RefererMessageId: x.GetRefererMessageId(),
+		Data:             x.GetData(),
+	}
 }
